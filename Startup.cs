99d2d9e28ba92f6482901369af0ef9792f8152d9
LauraZ <zaharialaura9@gmail.com>
2022-03-29 17:01:17 +0100

    Add database context to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,1 +1,37 @@
-// comment
+using Microsoft.EntityFrameworkCore;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<AppointmentDbContext>(options =>
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        
+        
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+        
+        app.UseCors("AllowAllOrigins");
+
+        app.UseEndpoints((endpoints => { endpoints.MapControllers();}));
+
+    }
+}
