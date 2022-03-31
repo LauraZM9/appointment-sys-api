ec9250b5dfc9d1f4771d75db226b66650db5895a
aamircodes <aamirp1@outlook.com>
2022-03-31 14:44:21 +0100

    Create factories and Gateways interface

---
+++ Factories/BookingFactory.cs
@@ -0,0 +1,19 @@
+using System;
+using appointment_sys_api.Domain;
+using appointment_sys_api.Infrastructure;
+
+namespace appointment_sys_api.Factories;
+
+public static class BookingFactory
+{
+    public static Booking ToDomain(this Booking entity)
+    {
+        return new Booking
+        {
+            Id = entity.Id;
+            CreatedAt = entity.CreatedAt;
+            Name = entity.Name;
+
+        };
+    }
+}
