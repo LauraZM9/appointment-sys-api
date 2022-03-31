a281b219fd8841821c68c32313375dd022153dbe
LauraZ <zaharialaura9@gmail.com>
2022-03-31 12:10:36 +0100

    Add domain directory and user class

---
+++ Domain/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace appointment_sys_api.Domain;
+
+public class User
+{
+  public int Id {get; set;}
+  public string FirstName {get; set;}
+  public string LastName {get; set;}
+}
