24b382d157c2f499651c9f3b2c12f77242bb6ab8
aamircodes <aamirp1@outlook.com>
2022-04-05 10:50:44 +0100

    Update IBookingsGateway interface

--- Gateways/Interfaces/IBookingsGateway.cs
+++ Gateways/Interfaces/IBookingsGateway.cs
@@ -6,4 +6,5 @@ namespace appointment_sys_api.Gateways.Interfaces;
 public interface IBookingsGateway
 {
     public Booking FindBooking(int id);
+    public Booking CreateBooking(Booking request);
 }
