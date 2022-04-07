8b81d78f5f4c44408bb51a478fc581b796e9573b
LauraZ <zaharialaura9@gmail.com>
2022-04-07 11:05:28 +0100

    Add connection string for the database on Heroku

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,7 @@ public class Startup
     {
       // database context
       services.AddDbContext<BookingDbContext>(options =>
-        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+        options.UseNpgsql(GetHerokuConnectionString()));
 
       // gateway
       services.AddScoped<IBookingsGateway, BookingsGateway>();
@@ -28,6 +28,19 @@ public class Startup
       services.AddControllers();
       services.AddEndpointsApiExplorer();
       services.AddSwaggerGen();
+    }
+
+    private string GetHerokuConnectionString() {
+        // Get the connection string from the ENV variables
+        string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        // parse the connection string
+        var databaseUri = new Uri(connectionUrl);
+
+        string db = databaseUri.LocalPath.TrimStart('/');
+        string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+        return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
