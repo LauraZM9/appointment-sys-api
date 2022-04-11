8be3d8d05de42dbbfc1479e77c118d9ea18eba2d
LauraZ <zaharialaura9@gmail.com>
2022-04-11 13:45:36 +0300

    Delete the dir outside

--- Infrastructure/Interfaces/IEntity.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace appointment_sys_api.Infrastructure.Interfaces;
-
-public interface IEntity
-{
-    public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
